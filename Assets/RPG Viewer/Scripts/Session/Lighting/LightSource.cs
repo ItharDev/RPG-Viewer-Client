5108fb164d99deb390b02e11c30c44c8457cfba6
Ithar <aleksi.soini2004@gmail.com>
2024-02-24 15:02:05 +0200

    Fixed lighting issues

--- Assets/RPG Viewer/Scripts/Session/Lighting/LightSource.cs
+++ Assets/RPG Viewer/Scripts/Session/Lighting/LightSource.cs
@@ -12,7 +12,8 @@ namespace RPG
         [SerializeField] private Light2D secondary;
         private TimerHelper primaryTimer;
         private TimerHelper secondaryTimer;
-        private float lightAlpha;
+        private float primaryAlpha;
+        private float secondaryAlpha;
 
         public void LoadData(PresetData _data)
         {
@@ -21,26 +22,25 @@ namespace RPG
             // Primary
             primary.size = data.primary.radius / Session.Instance.Grid.Unit.scale * Session.Instance.Grid.CellSize + Session.Instance.Grid.CellSize * 0.5f;
             primary.color = data.primary.color;
-            lightAlpha = data.primary.color.a;
+            primaryAlpha = data.primary.color.a;
 
             primary.spotAngleInner = Mathf.Clamp(data.primary.angle, 0.0f, 360.0f);
             primary.spotAngleOuter = Mathf.Clamp(data.primary.angle + 10.0f, 0.0f, 360.0f);
 
-            StopAllCoroutines();
-            primaryTimer = TimerHelper.Create();
-            if (data.primary.effect.frequency > 0 && data.primary.effect.type == 2) StartCoroutine(PulseCoroutine());
-
             // Secondary
             secondary.size = data.secondary.radius / Session.Instance.Grid.Unit.scale * Session.Instance.Grid.CellSize + Session.Instance.Grid.CellSize * 0.5f;
             secondary.color = data.secondary.color;
-            lightAlpha = data.secondary.color.a;
+            secondaryAlpha = data.secondary.color.a;
 
             secondary.spotAngleInner = Mathf.Clamp(data.secondary.angle, 0.0f, 360.0f);
             secondary.spotAngleOuter = Mathf.Clamp(data.secondary.angle + 10.0f, 0.0f, 360.0f);
 
-            StopAllCoroutines();
+            primaryTimer = TimerHelper.Create();
             secondaryTimer = TimerHelper.Create();
-            if (data.secondary.effect.frequency > 0 && data.secondary.effect.type == 2) StartCoroutine(PulseCoroutine());
+
+            StopAllCoroutines();
+            if (data.primary.effect.frequency > 0 && data.primary.effect.type == 2) StartCoroutine(PrimaryPulse());
+            if (data.secondary.effect.frequency > 0 && data.secondary.effect.type == 2) StartCoroutine(SecondaryPulse());
         }
         public void Toggle(bool enabled)
         {
@@ -60,7 +60,7 @@ namespace RPG
             {
                 if (primaryTimer.GetMillisecs() > 1000f / data.primary.effect.frequency)
                 {
-                    float tempAlpha = lightAlpha;
+                    float tempAlpha = primaryAlpha;
                     tempAlpha += Random.Range(-data.primary.effect.strength * 0.01f, data.primary.effect.strength * 0.01f);
                     primary.color.a = tempAlpha;
                     primaryTimer.Reset();
@@ -77,7 +77,7 @@ namespace RPG
             {
                 if (secondaryTimer.GetMillisecs() > 1000f / data.secondary.effect.frequency)
                 {
-                    float tempAlpha = lightAlpha;
+                    float tempAlpha = secondaryAlpha;
                     tempAlpha += Random.Range(-data.secondary.effect.strength * 0.01f, data.secondary.effect.strength * 0.01f);
                     secondary.color.a = tempAlpha;
                     secondaryTimer.Reset();
@@ -85,52 +85,54 @@ namespace RPG
             }
         }
 
-        private IEnumerator PulseCoroutine()
+        private IEnumerator PrimaryPulse()
         {
-            // primary
-            float primaryAlpha = primary.color.a - data.primary.effect.strength * 0.01f;
-            float originalPrimary = primary.color.a;
-            float primaryTime = data.primary.effect.frequency * 0.5f;
+            float originalAlpha = primary.color.a - data.primary.effect.strength * 0.01f;
+            float originalColor = primary.color.a;
+            float time = data.primary.effect.frequency * 0.5f;
 
-            for (float t = 0f; t < primaryTime; t += Time.deltaTime)
+            for (float t = 0f; t < time; t += Time.deltaTime)
             {
-                float normalizedTime = t / primaryTime;
-                primary.color.a = Mathf.Lerp(originalPrimary, primaryAlpha, normalizedTime);
+                float normalizedTime = t / time;
+                primary.color.a = Mathf.Lerp(originalColor, originalAlpha, normalizedTime);
                 yield return null;
             }
-            primary.color.a = primaryAlpha;
+            primary.color.a = originalAlpha;
 
-            for (float t = 0f; t < primaryTime; t += Time.deltaTime)
+            for (float t = 0f; t < time; t += Time.deltaTime)
             {
-                float normalizedTime = t / primaryTime;
-                primary.color.a = Mathf.Lerp(primaryAlpha, originalPrimary, normalizedTime);
-                yield return null;
-            }
-
-            primary.color.a = originalPrimary;
-
-            // Secondary
-            float secondaryAlpha = secondary.color.a - data.secondary.effect.strength * 0.01f;
-            float originalSecondary = secondary.color.a;
-            float secondaryTime = data.secondary.effect.frequency * 0.5f;
-
-            for (float t = 0f; t < secondaryTime; t += Time.deltaTime)
-            {
-                float normalizedTime = t / secondaryTime;
-                secondary.color.a = Mathf.Lerp(originalSecondary, secondaryAlpha, normalizedTime);
-                yield return null;
-            }
-            secondary.color.a = secondaryAlpha;
-
-            for (float t = 0f; t < secondaryTime; t += Time.deltaTime)
-            {
-                float normalizedTime = t / secondaryTime;
-                secondary.color.a = Mathf.Lerp(secondaryAlpha, originalSecondary, normalizedTime);
+                float normalizedTime = t / time;
+                primary.color.a = Mathf.Lerp(originalAlpha, originalColor, normalizedTime);
                 yield return null;
             }
 
-            secondary.color.a = originalSecondary;
-            StartCoroutine(PulseCoroutine());
+            primary.color.a = originalColor;
+
+            StartCoroutine(PrimaryPulse());
+        }
+        private IEnumerator SecondaryPulse()
+        {
+            float originalAlpha = secondary.color.a - data.secondary.effect.strength * 0.01f;
+            float originalColor = secondary.color.a;
+            float time = data.secondary.effect.frequency * 0.5f;
+
+            for (float t = 0f; t < time; t += Time.deltaTime)
+            {
+                float normalizedTime = t / time;
+                secondary.color.a = Mathf.Lerp(originalColor, originalAlpha, normalizedTime);
+                yield return null;
+            }
+            secondary.color.a = originalAlpha;
+
+            for (float t = 0f; t < time; t += Time.deltaTime)
+            {
+                float normalizedTime = t / time;
+                secondary.color.a = Mathf.Lerp(originalAlpha, originalColor, normalizedTime);
+                yield return null;
+            }
+
+            secondary.color.a = originalColor;
+            StartCoroutine(SecondaryPulse());
         }
     }
 }
